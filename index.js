6616f90aa5863c20d044262fc0cdc6aac2ba2b07
Matt Harrison <hi@matt-harrison.com>
2015-06-15 00:00:55 +0800

    Refactoring

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ var Hoek = require('hoek');
 var internals = {};
 
 
+internals.Request = function (options, request, reply) {
+
+	var route = this.route = request.route;
+	this.pluginOptions = options;
+	this.request = request;
+	this.reply = reply;
+	this.routeOptions = route.settings.plugins[internals.attributes.name];
+	this.origFailAction = route.settings.validate.failAction;
+	route.settings.validate.failAction = this.failAction.bind(this);
+
+	reply.continue();
+};
+
+
+internals.Request.prototype.failAction = function (request, reply, source, error) {
+
+    var self = this;
+    var failArgs = arguments;
+    this.source = source;
+    this.error = error;
+
+    var next = function () {
+
+        if (typeof self.origFailAction === 'function') {
+            return self.origFailAction.apply(self, failArgs);
+        }
+
+        return reply(this.error);
+    };
+
+    this.messages = this.routeOptions[source];
+
+    if (!this.messages) {
+        next();
+    }
+
+    this.doReplacements();
+    this.wrapError();
+
+    next();
+};
+
+
 internals.interpolate = exports.interpolate = function (str, context) {
 
     if (!context) {
@@ -22,8 +65,16 @@ internals.interpolate = exports.interpolate = function (str, context) {
 };
 
 
-internals.doReplacements = function (error, messages) {
+internals.getBestLanguageMatch = function (request, rules) {
+	return 'en';
+};
 
+
+internals.Request.prototype.doReplacements = function () {
+
+	var request = this.request;
+	var error = this.error;
+	var messages = this.messages;
     var details = error.data.details;
 
     for (var i = 0; i < details.length; i++) {
@@ -38,7 +89,7 @@ internals.doReplacements = function (error, messages) {
                 }
 
                 if (typeof rules[err.type] === 'object') {
-                    var lang = 'zh';
+                    var lang = internals.getBestLanguageMatch(request, rules);
                     err.message = internals.interpolate(rules[err.type][lang], err.context);
                 }
 
@@ -46,12 +97,13 @@ internals.doReplacements = function (error, messages) {
         }
     }
 
-    return error;
 };
 
 
-internals.wrapError = function (err, source) {
+internals.Request.prototype.wrapError = function () {
 
+	var err = this.error.data;
+	var source = this.source;
     var error = Boom.badRequest(err.details[0].message, err);
     error.output.payload.validation = { source: source, keys: [] };
     if (err.details) {
@@ -59,51 +111,27 @@ internals.wrapError = function (err, source) {
             error.output.payload.validation.keys.push(Hoek.escapeHtml(err.details[i].path));
         }
     }
-
-    return error;
 };
 
 
 internals.onPostAuth = function (request, reply) {
 
-    var route = request.route;
-    var options = request.route.settings.plugins[internals.attributes.name];
-
-    var origFailAction = route.settings.validate.failAction;
-
-    route.settings.validate.failAction = function (request, reply, source, error) {
-
-        var self = this;
-        var failArgs = arguments;
-
-        var next = function () {
-
-            if (typeof origFailAction === 'function') {
-                return origFailAction.apply(self, failArgs);
-            }
-
-            return reply(error);
-        };
-
-        var messages = options[source];
-
-        if (!messages) {
-            next();
-        }
-
-        error = internals.doReplacements(error, messages);
-        error = internals.wrapError(error.data, source);
-
-        next();
-    };
-
-    reply.continue();
+    return new internals.Request(this.options, request, reply);
 };
 
 
 exports.register = function (server, options, next) {
 
-    server.ext('onPostAuth', internals.onPostAuth);
+	var defaults = {
+		defaultLang: 'en'
+	};
+
+	server.ext('onPostAuth', internals.onPostAuth, {
+		bind: {
+			options: Hoek.applyToDefaults(defaults, options)
+		}
+	});
+
     next();
 };
 
