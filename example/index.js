4408e50c522784c405ae35be8854e60cd7a288d9
Matt Harrison <hi@matt-harrison.com>
2015-06-14 19:51:10 +0800

    Renamed to enjoi

--- example/index.js
+++ example/index.js
@@ -1,13 +1,60 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 
 var server = new Hapi.Server();
 server.connection({ port: 4000 });
 
 server.route({
+	config: {
+		validate: {
+			params: {
+				perp: Joi.string().regex(/moonlight|boogie/).required()
+			},
+			failAction: function (request, reply, source, error) {
+
+				console.log(error.data.details);
+
+				reply(error);
+			}
+		},
+		plugins: {
+			enjoi: {
+				params: {
+					perp: {
+						'string.regex.base': {
+							en: 'Must be one of "moonlight" or "boogie"',
+							zh: '必定為其中之一 "moonlight" 或 "boogie"'
+						},
+						'any.required': {
+							en: 'Forget something?',
+							zh: '遺忘了什麼嗎？'
+						}
+					}
+				}
+			}
+		},
+	},
 	method: 'GET',
-	path: '/',
+	path: '/blame/{perp?}',
 	handler: function (request, reply) {
 
 		reply('Ok!');
 	}
 });
+
+server.register([
+	{
+		register: require('../'),
+		options: {
+			defaultLang: 'en'
+		}
+	}
+], function (err) {
+
+	if (err) {
+		throw err;
+	}
+
+	server.start(function () {
+	});
+})
---
+++ index.js
@@ -0,0 +1,69 @@
+var Boom = require('boom');
+var Hoek = require('hoek');
+
+exports.register = function (server, options, next) {
+
+	server.ext('onPostAuth', function (request, reply) {
+
+		var route = request.route;
+		var options = request.route.settings.plugins.enjoi;
+
+		var originalFailAction = route.settings.validate.failAction;
+
+		route.settings.validate.failAction = function (request, reply, source, error) {
+
+			var messages = options[source];
+
+			// If no custom messages have been supplied fallback to the custom fail action
+
+			if (!messages) {
+				if (typeof originalFailAction === 'function') {
+					return originalFailAction.apply(this, arguments);
+				}
+
+				return reply(error);
+			}
+
+			var details = error.data.details;
+
+			for (var i = 0; i < details.length; i++) {
+				var err = details[i];
+				var path = err.path;
+				if (messages.hasOwnProperty(path)) {
+					var rules = messages[path];
+					if (rules.hasOwnProperty(err.type)) {
+						var lang = 'zh';
+						err.message = rules[err.type][lang];
+					}
+				}
+			}
+
+			var err = error.data;
+
+			var error = Boom.badRequest(err.details[0].message, err);
+			error.output.payload.validation = { source: source, keys: [] };
+			if (err.details) {
+			    for (var i = 0, il = err.details.length; i < il; ++i) {
+			        error.output.payload.validation.keys.push(Hoek.escapeHtml(err.details[i].path));
+			    }
+			}
+
+			if (typeof originalFailAction === 'function') {
+				return originalFailAction.apply(this, arguments);
+			}
+
+			return reply(error);
+		};
+
+		reply.continue();
+	});
+
+	next();
+};
+
+var Package = require('./package');
+
+exports.register.attributes = {
+	name: Package.name,
+	version: Package.version
+};
